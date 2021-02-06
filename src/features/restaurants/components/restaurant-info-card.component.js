a2a68650881862661d46886d174dbbde0c84454b
Abhishek Kumar <abhishekkr95.dev@gmail.com>
2021-02-06 22:03:57 +0530

    Added Rating Component

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components/native";
 import { StyleSheet, Text } from "react-native";
 import { Card } from "react-native-paper";
+import { SvgXml } from "react-native-svg";
+import star from "../../../../assets/star";
 
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
@@ -12,9 +14,24 @@ const RestaurantCardCover = styled(Card.Cover)`
   background-color: white;
 `;
 
+const Address = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
 const Title = styled.Text`
-  padding: 16px;
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.body};
   color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
 `;
 
 const RestaurantInfoCard = ({ restaurant = {} }) => {
@@ -29,10 +46,20 @@ const RestaurantInfoCard = ({ restaurant = {} }) => {
     rating = 4,
     isClosedTemporarily,
   } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
-      <Title>{name}</Title>
+      <Info>
+        <Title>{name}</Title>
+        <Rating>
+          {ratingArray.map(() => (
+            <SvgXml xml={star} width={20} height={20} />
+          ))}
+        </Rating>
+        <Address>{address}</Address>
+      </Info>
     </RestaurantCard>
   );
 };
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -17,7 +17,6 @@ const SearchContainer = styled.View`
 const RestaurantListContainer = styled.View`
   flex: 1;
   padding: 16px;
-  background-color: blue;
 `;
 
 const RestaurantsScreen = () => {
