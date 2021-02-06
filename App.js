b450a26be613069e6db58ef534280236dfa0d112
Abhishek Kumar <abhishekkr95.dev@gmail.com>
2021-02-06 16:38:36 +0530

    Added Styled Components

--- App.js
+++ App.js
@@ -1,23 +1,13 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
-import { StatusBar, SafeAreaView, StyleSheet, Text, View, Platform } from "react-native";
 
-const isAndroid = Platform.OS === "android";
+import RestaurantsScreen from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
   return (
     <React.Fragment>
-      <SafeAreaView style={{ flex: 1, marginTop: isAndroid ? StatusBar.currentHeight : 0 }}>
-        <View style={{ padding: 16, backgroundColor: "green" }}>
-          <Text>search</Text>
-        </View>
-        <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
-          <Text>list</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen />
       <ExpoStatusBar style="auto" />
     </React.Fragment>
   );
 }
-
-const styles = StyleSheet.create({});
---
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { SafeAreaView, Platform, StatusBar } from "react-native";
+import { Searchbar } from "react-native-paper";
+import styled from "styled-components/native";
+
+import RestaurantInfoCard from "../components/restaurant-info-card.component";
+const isAndroid = Platform.OS === "android";
+
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  ${isAndroid && `margin-top:${StatusBar.currentHeight}px`}
+`;
+
+const SearchContainer = styled.View`
+  padding: 16px;
+`;
+const RestaurantListContainer = styled.View`
+  flex: 1;
+  padding: 16px;
+  background-color: blue;
+`;
+
+const RestaurantsScreen = () => {
+  return (
+    <SafeArea>
+      <SearchContainer>
+        <Searchbar />
+      </SearchContainer>
+      <RestaurantListContainer>
+        <RestaurantInfoCard />
+      </RestaurantListContainer>
+    </SafeArea>
+  );
+};
+
+export default RestaurantsScreen;
